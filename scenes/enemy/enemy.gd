7d523e7d1a28bbace672086a45720a2c58b30489
kacperpucylo <pucylek@wp.pl>
2024-01-13 18:47:50 +0100

    restarted for the 3rd time, player movement and basic decay working

--- scenes/enemy/enemy.gd
+++
@@ -1,9 +0,0 @@
-extends CharacterBody2D
-
-@onready var vision_cone: Area2D = $VisionCone
-
-func _ready():
-	vision_cone.area_entered.connect(on_vision_cone_area_entered)
-
-func on_vision_cone_area_entered(_area: Area2D):
-	print("entered")
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,19 +1,38 @@
-extends CharacterBody2D
+extends Area2D
 
-@export var movement_speed = 300
-@onready var bite = $Bite
 
-func _physics_process(_delta):
-	var movement_x = Input.get_axis("move_left", "move_right")
-	var movement_y = Input.get_axis("move_up", "move_down")
+@onready var decay_scene: PackedScene = preload("res://scenes/decay/decay.tscn")
+@onready var ray: RayCast2D = $RayCast2D
 
-	rotation = deg_to_rad(-movement_x * 90)
-	# rotate(deg_to_rad(movement_x * 90 + movement_y * 180))
-	print(movement_x * 90)
-	print(movement_y * 180)
+var tile_size = 16
+var inputs = {
+	"right": Vector2.RIGHT,
+	"left": Vector2.LEFT,
+	"up": Vector2.UP,
+	"down": Vector2.DOWN
+}
 
-	if Input.is_action_just_pressed("attack"):
-		bite.attack()
+func _ready():
+	global_position = global_position.snapped(Vector2.ONE * tile_size)
+	global_position += Vector2.ONE * tile_size / 2.0
 
-	velocity = Vector2(movement_x, movement_y).normalized() * movement_speed
-	move_and_slide()
+func _process(_delta):
+	if Input.is_action_just_pressed("restart"):
+		get_tree().reload_current_scene()
+
+func _unhandled_input(event):
+	for dir in inputs.keys():
+		if event.is_action_pressed(dir):
+			move(dir)
+
+func move(dir):
+	ray.target_position = inputs[dir] * tile_size
+	ray.force_raycast_update()
+	if !ray.is_colliding():
+		var old_global_position = global_position
+		global_position += inputs[dir] * tile_size
+		var decay_instance = decay_scene.instantiate()
+		decay_instance.global_position = old_global_position
+		print("Parent global_position: ", get_node("/root/Main").global_position)
+		print("Decay instance global_position: ", decay_instance.global_position)
+		get_node("/root/Main").add_child(decay_instance)
